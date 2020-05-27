53fb81fd56fd9d5ca5845496f9c9138b7cbf9731
bojue <lansemaikefeng@126.com>
2020-05-27 14:01:23 +0800

    集成dev-server

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     index: './src/index.js'
   },
   output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath:'/dist/',
     filename: 'js/bundle.js',
-    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -14,7 +17,16 @@ module.exports = {
         test: /\.(js|jsx)$/,
         use: 'babel-loader',
         exclude: /node_modules/,
-      }
+      },
+      {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+      {test: /\.(eot|svg|ttf|woff|woff2|png|ico)\w*/, use: 'file-loader'},
     ]
-  }
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, './public/index.html'),
+      filename:'index.html',
+      favicon:'./public/favicon.ico'
+    })
+  ]
 }
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.base.config.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
+  watch:true,
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    hot:true,
+    compress: true,
+    open:"Chrome",
+    openPage:'index.html'
+  },
 });
